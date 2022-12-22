acbf20ca46eb101b0b1c51cdb66eb526034ef729
im4i <rodrigoimai@hotmail.com>
2022-12-22 08:57:06 -0300

    Configuração do projeto e criação de entidade e endpoint

---
+++ agencia-app/src/main/java/com/imai/cliente/Cliente.java
@@ -0,0 +1,12 @@
+package com.imai.cliente;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Cliente extends PanacheEntity {
+
+    public String nome;
+    
+}
